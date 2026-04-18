7fcbf90e1a2fe6aee0fb2b86cf584e1f6462d455
agimle <a1480251819@icloud.com>
2026-04-18 13:12:19 +0800

    feat: 添加ModEntry初始化功能
    实现ModEntry类的初始化功能，包括Harmony补丁加载和脚本管理

--- Scripts/ModEntry.cs
+++ Scripts/ModEntry.cs
@@ -1,6 +1,21 @@
-﻿namespace AttackLog.Scripts;
+﻿using Godot.Bridge;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Logging;
+using MegaCrit.Sts2.Core.Modding;
 
-public class Entry
+namespace AttackLog;
+
+public class ModEntry
 {
-    
+    // 初始化函数
+    public static void Init()
+    {
+        // 打patch（即修改游戏代码的功能）用
+        // 传入参数随意，只要不和其他人撞车即可
+        var harmony = new Harmony(".luting4u.sts2.attacklog");
+        harmony.PatchAll();
+        // 使得tscn可以加载自定义脚本
+        ScriptManagerBridge.LookupScriptsInAssembly(typeof(ModEntry).Assembly);
+        Log.Info("Mod initialized!");
+    }
 }
